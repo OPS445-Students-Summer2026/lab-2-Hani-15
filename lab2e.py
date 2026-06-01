2839285352e3f78dcfc193fa2d1add19b89d98fd
Hani Ali <hali154@myseneca.ca>
2026-06-01 04:52:03 -0400

    Added lab2e

---
+++ lab2e.py
@@ -0,0 +1,14 @@
+#!/usr/bin/env python3
+
+#Author: Hani Alamin Ali
+
+#Author ID: hali154
+
+#Date Created: 2026/06/01
+
+timer = 10
+
+while timer != 0:
+    print(timer)
+    timer = timer - 1
+print('blast off!')
