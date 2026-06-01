27ac368723ad2d719f85851b5ee5127b7b3ef58a
Hani Ali <hali154@myseneca.ca>
2026-06-01 04:52:47 -0400

    Added lab2g

---
+++ lab2g.py
@@ -0,0 +1,20 @@
+#!/usr/bin/env python3
+
+#Author: Hani Alamin Ali
+
+#Author ID: hali154
+
+#Date Created: 2026/06/01
+
+import sys
+
+
+if len(sys.argv) == 1:
+    timer = 3
+else:
+    timer = int(sys.argv[1])
+
+while timer != 0:
+        print(timer)
+        timer = timer - 1
+print('blast off!')
