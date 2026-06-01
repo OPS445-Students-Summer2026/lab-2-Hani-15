d5524e7b9b9c65a5530609463df634c8206b01fa
Hani Ali <hali154@myseneca.ca>
2026-06-01 04:52:29 -0400

    Added lab2f

---
+++ lab2f.py
@@ -0,0 +1,16 @@
+#!/usr/bin/env python3
+
+#Author: Hani Alamin Ali
+
+#Author ID: hali154
+
+#Date Created: 2026/06/01
+
+import sys
+
+timer = int(sys.argv[1])
+
+while timer != 0:
+    print(timer)
+    timer = timer - 1
+print('blast off!')
